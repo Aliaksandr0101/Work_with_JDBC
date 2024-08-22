6c772a8b44fd2bcd7f5fb4dc07e48fc9a95a5d74
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-08-23 00:02:42 +0300

    Second commit. Create class ExampleTwo and made a connection to the database pets and add one pet in table.

---
+++ src/ExampleTwo.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class ExampleTwo {
+
+    public static void main(String[] args) throws SQLException {
+        Class<Driver> driverClass = Driver.class;
+        String password = "sasha";
+        String userName = "postgres";
+        String url = "jdbc:postgresql://localhost:5432/Fierst_Database";
+        /*DriverManager.getConnection(url, userName, password);*/
+
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+
+
+          connection = DriverManager.getConnection(url, userName, password);
+          System.out.println("Соединение с базой данных установлено.");
+          String insertPet = "INSERT INTO pets " +
+                  "(pets_id, kind_of_animal, name_of_animal, color_of_animal, age_of_animal) VALUES (?, ?, ? , ?, ?)";
+        preparedStatement = connection.prepareStatement(insertPet);
+        // Первое животное
+        preparedStatement.setInt(1, 1);
+        preparedStatement.setString(2, "Собака");
+        preparedStatement.setString(3, "Барс");
+        preparedStatement.setString(4, "Серый");
+        preparedStatement.setInt(5, 3);
+        preparedStatement.executeUpdate();
+
+    }
+}
+
