d2c5b6693937c27facc9ad593147164f5906ee41
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-08-26 01:21:54 +0300

    Five commit. Create a table of pets, add to the table "pets"  three animals, output animals that are less than 8 years old.

--- src/JDBS_first_class.java
+++ src/JDBS_first_class.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBS_first_class {
 
@@ -12,9 +9,38 @@ public class JDBS_first_class {
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
         Connection connection = null;
+
+        String createTable = "CREATE TABLE IF NOT EXISTS pets ( " +
+                " pets_id INT PRIMARY KEY, " +
+                " kind_of_animal VARCHAR(50), " +
+                " name_of_animal VARCHAR(50), " +
+                " color_of_animal VARCHAR(50), " +
+                " age_of_animal INT ) " ;
+
+        String insertPets = "INSERT INTO pets OVERRIDING SYSTEM VALUE VALUES " +
+                "(1, 'dog', 'Barsik', 'black', 4 )," +
+                " (2,'horse', 'Iva', 'white', 6 )," +
+                "(3, 'duck', 'Happy', 'grey', 9 )" ;
+
+        String output = "SELECT pets_id, kind_of_animal, name_of_animal, color_of_animal, age_of_animal FROM pets where age_of_animal < 8";
+
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println(connection.getTransactionIsolation());
+            Statement statement = connection.createStatement();
+            statement.execute(createTable);
+            statement.execute(insertPets);
+            System.out.println("Number of pets added = " + statement.getUpdateCount());
+
+            ResultSet resultSet = statement.executeQuery(output);
+            while (resultSet.next()) {
+                System.out.print(resultSet.getInt(1) + " - ");
+                System.out.print(resultSet.getString(2) + " - ");
+                System.out.print(resultSet.getString(3) + " - ");
+                System.out.print(resultSet.getString(4) + " - ");
+                System.out.println(resultSet.getInt(5));
+            }
+
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
